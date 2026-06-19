f968edbc73efb3431ac1d63010655781107967b6
Lokesh Setty <lokeshsetty10@gmail.com>
2026-06-18 20:03:11 -0400

    Initial Commit

---
+++ ORB/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ORB
+//
+//  Created by Lokesh Setty Kalakonda on 6/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ORB/ORBApp.swift
@@ -0,0 +1,17 @@
+//
+//  ORBApp.swift
+//  ORB
+//
+//  Created by Lokesh Setty Kalakonda on 6/18/26.
+//
+
+import SwiftUI
+
+@main
+struct ORBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
